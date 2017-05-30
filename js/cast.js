dbbdd5e84827f5c152e4528741cfaf051708ef03
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-05-29 19:03:47 -0600

    Fixed the CSS and added more functionality

--- js/cast.js
+++ js/cast.js
@@ -19,15 +19,28 @@ var jqxhr = $.getJSON( "../js/json/cast.json", function(data) {
     jsonError();
 })
 
+// Extend sting prototype methods
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
 function jsonSuccess(data) {
     var castObj = data.cast;
-    castObj.forEach(function(element) {
-        var h2 = $('<h2></h2>');
-        h2.text(element.name);
-        h2.click(function(){
-            castBio.html(element.bio);
+    castObj.forEach(function(element, index) {
+        var h3 = $('<h3></h3>');
+        var br = document.createElement('br');
+        h3.html(element.name);
+        h3.click(function(){
+            var h3Array = $(castMember)[0].getElementsByTagName('h3');
+            castBio.html(element.bio.replaceAll('/n', '</br>' ));        
+            for(var i = 0; i < h3Array.length; i++) {
+                h3Array[i].classList.remove('text-selected');
+                h3Array[index].classList.add('text-selected');
+            }
+            // for every h3 resets the class and only add the font size to the current h3
         })
-        castMember.append(h2);
+        castMember.append(h3);
     });
     data;
 }
