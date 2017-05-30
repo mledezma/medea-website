faff618a11ea0e00a860b6872daed3cc3fefee13
Marco Ledezma <marcoledezmacordero09@gmail.com>
2017-05-30 13:02:56 -0600

    fixed the js

--- js/crew_EN.js
+++ js/crew_EN.js
@@ -1,11 +1,11 @@
-var castSingle = (function ($) {
+var crewSingle = (function ($) {
 
     // Instance stores a reference to the Singleton
     var instance;
 
     function init() {
-        var castMember = $('#cast');
-        var castBio = $('#castBio');
+        var crewMember = $('#crew');
+        var crewBio = $('#crewBio');
 
         var jqxhr = $.getJSON( "../js/json/crew_EN.json", function(data) {
             jsonSuccess(data);
@@ -22,28 +22,28 @@ var castSingle = (function ($) {
 
         // Triggered if the JSON succeed
         function jsonSuccess(data) {
-            var castObj = data.cast;
-            castObj.forEach(function(element, index) {
+            var crewObj = data.crew;
+            crewObj.forEach(function(element, index) {
                 var h3 = $('<h3></h3>');
                 var br = document.createElement('br');
                 h3.html(element.name);
                 h3.click(function(){
-                    var h3Array = $(castMember)[0].getElementsByTagName('h3');
-                    castBio.html(element.bio.replaceAll('/n', '</br>' ));        
+                    var h3Array = $(crewMember)[0].getElementsByTagName('h3');
+                    crewBio.html(element.bio.replaceAll('/n', '</br>' ));        
                     for(var i = 0; i < h3Array.length; i++) {
                         h3Array[i].classList.remove('text-selected');
                         h3Array[index].classList.add('text-selected');
                     }
                     // for every h3 resets the class and only add the font size to the current h3
                 })
-                castMember.append(h3);
+                crewMember.append(h3);
             });
             data;
         }
 
         // Triggered if the JSON fails
         function jsonError() {
-            castBio.html('Lo sentimos pero la página no cargó correctamente, inténtelo de nuevo más tarde');    
+            crewBio.html('Lo sentimos pero la página no cargó correctamente, inténtelo de nuevo más tarde');    
         }
     };
 
@@ -60,4 +60,4 @@ var castSingle = (function ($) {
     };
 })(jQuery);
 
-castSingle.getInstance();
+crewSingle.getInstance();
